683efd65a41f90af57b2cdfe8830bf25269bf338
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-05-10 20:42:06 +0200

    More sparkly

--- src/Main.elm
+++ src/Main.elm
@@ -69,7 +69,7 @@ init _ =
 
 wordDuration : Duration
 wordDuration =
-    Duration.seconds 3
+    Duration.seconds 1
 
 
 wordsCount : Int
@@ -256,35 +256,24 @@ fragmentShader =
 
         // https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
         // BEGIN
-        float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
-        vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
-        vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+        float rand(vec2 n) { 
+            return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+        }
 
-        float noise(vec3 p){
-            vec3 a = floor(p);
-            vec3 d = p - a;
-            d = d * d * (3.0 - 2.0 * d);
-
-            vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
-            vec4 k1 = perm(b.xyxy);
-            vec4 k2 = perm(k1.xyxy + b.zzww);
-
-            vec4 c = k2 + a.zzzz;
-            vec4 k3 = perm(c);
-            vec4 k4 = perm(c + 1.0);
-
-            vec4 o1 = fract(k3 * (1.0 / 41.0));
-            vec4 o2 = fract(k4 * (1.0 / 41.0));
-
-            vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-            vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
-
-            return o4.y * d.y + o4.x * (1.0 - d.y);
+        float noise(vec2 p){
+            vec2 ip = floor(p);
+            vec2 u = fract(p);
+            u = u*u*(3.0-2.0*u);
+            
+            float res = mix(
+                mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+                mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+            return res*res;
         }
         // END
 
         void main () {
-            gl_FragColor = vec4(.2,.2,.7,1) * noise(319. * vpos);
+            gl_FragColor = vec4(.2,.2,.7,1) * noise(3197. * vpos.xy * vpos.z);
         }
     |]
 
@@ -311,36 +300,25 @@ backgroundFragmentShader =
 
         // https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
         // BEGIN
-        float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
-        vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
-        vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+        float rand(vec2 n) { 
+            return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+        }
 
-        float noise(vec3 p){
-            vec3 a = floor(p);
-            vec3 d = p - a;
-            d = d * d * (3.0 - 2.0 * d);
-
-            vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
-            vec4 k1 = perm(b.xyxy);
-            vec4 k2 = perm(k1.xyxy + b.zzww);
-
-            vec4 c = k2 + a.zzzz;
-            vec4 k3 = perm(c);
-            vec4 k4 = perm(c + 1.0);
-
-            vec4 o1 = fract(k3 * (1.0 / 41.0));
-            vec4 o2 = fract(k4 * (1.0 / 41.0));
-
-            vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
-            vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
-
-            return o4.y * d.y + o4.x * (1.0 - d.y);
+        float noise(vec2 p){
+            vec2 ip = floor(p);
+            vec2 u = fract(p);
+            u = u*u*(3.0-2.0*u);
+            
+            float res = mix(
+                mix(rand(ip),rand(ip+vec2(1.0,0.0)),u.x),
+                mix(rand(ip+vec2(0.0,1.0)),rand(ip+vec2(1.0,1.0)),u.x),u.y);
+            return res*res;
         }
         // END
 
         void main () {
-            vec2 npos = 20000. * vec2(16, 9) * vpos;
-            gl_FragColor = vec4(.7,.7,.9,1) * (noise(vec3(npos, 0)) > 0.9 ? 1. : 0.);
+            vec2 npos = 41. * vec2(16, 9) * vpos;
+            gl_FragColor = vec4(.7,.7,.9,1) * (noise(npos) > 0.85 ? 1. : 0.);
         }
     |]
 
